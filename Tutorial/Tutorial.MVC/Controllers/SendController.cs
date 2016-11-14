176e18c5587a6561d600e8690a4df4c5c7291879
André Davis <CubicleJockey@users.noreply.github.com>
2016-11-14 15:39:17 -0800

    Created an SendController method to trigger server to send to HomeController

---
+++ Tutorial/Tutorial.MVC/Controllers/SendController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNet.SignalR;
+using System.Web.Mvc;
+using Tutorial.MVC.Models;
+using Tutorial.MVC.SignalR.Hubs;
+
+namespace Tutorial.MVC.Controllers
+{
+    /// <summary>
+    /// Callilng this controller Index is used to simulate
+    /// </summary>
+    public class SendController : Controller 
+    {
+        private readonly IHubContext hubContext;
+
+        public SendController()
+        {
+            hubContext = GlobalHost.ConnectionManager.GetHubContext<ComplexTypeHub>();
+        }
+
+
+        // GET: Send
+        public ActionResult Index()
+        {
+            IList<IAnimal> animals = new IAnimal[2] {new Chicken(), new Dog()};
+            hubContext.Clients.All.getAnimals(animals);
+            return null;
+        }
+    }
+}
